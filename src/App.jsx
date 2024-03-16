fc990f4485c7f93b62031dc8ec19fbef99925120
Nasghul01 <nasrul01arsyad@gmail.com>
2024-03-16 14:11:52 +0800

    add new function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState , useEffect} from 'react'
+import { useState , useEffect, useRef} from 'react'
 import blob1 from './assets/blob1.svg'
 import blob2 from './assets/blob2.svg'
 import Question from './components/Question.jsx'
@@ -6,40 +6,27 @@ import Question from './components/Question.jsx'
 function App() {
 
   const [quiz , setQuiz] = useState(false)
-  const [data, setData] = useState([])
 
-  useEffect(() => {
-      fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple")
-      .then(res => {
-        return res.json();
-      })
-      .then( loadQuestions => {
-        console.log(loadQuestions.results);
-        // loadQuestions.results.map (loadQuestions => {
-          setData(loadQuestions.results)
-      })
-      .catch( err => {
-        console.error(err);
-      })
-}, [quiz, true])
+  function handleGame() {
+    setQuiz(!quiz)
+  }
 
-
-
+  
 
   return (
     <main className='p-3 position-relative rounded d-flex'>
           <img src={blob1} alt="blob1" className='position-absolute top-0 end-0 rounded' />
           {
             quiz ?
-            <Question 
-            data = {data}/>
+            <Question
+            handleGameClick = {handleGame}/>
             :
             <div className='container d-flex flex-column justify-content-center align-items-center'>
               <h1 className=''>Quizzical</h1>
               <p>Some description if needed</p>
               <button 
                 className='btn btn-primary p-2 w-25 rounded' 
-                onClick= { () => setQuiz(true)} >
+                onClick= {handleGame} >
                 Quiz
               </button>
             </div>
--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,18 +1,47 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
+import { nanoid } from "nanoid";
 
 export default function Question (props){
-    // const [ question, incorect_answers , correct_answer ] = results
+    const [data, setData] = useState([])
+    const renderAfterCalled = useRef(false)
+
+    useEffect(() => {
+
+        if (!renderAfterCalled.current) {
+        fetch("https://opentdb.com/api.php?amount=5&category=9&difficulty=medium&type=multiple")
+        .then(res => {
+            return res.json();
+        })
+        .then( loadQuestions => {
+            console.log(loadQuestions.results);
+            const newTrivia = []
+            loadQuestions.results.forEach(question => newTrivia.push(
+                {
+                    id: nanoid(),
+                    answer: shuffledAnswer([question.incorrect_answers, question.correct_answer]),
+                    correct: question.correct_answer,
+                    selected: null,
+                    checked:false
+                }
+            ))
+            setData(newTrivia)
+            console.log(newTrivia)
+        })
+        .catch( err => {
+            console.error(err);
+        })
+        }
+        renderAfterCalled.current = true;
+        
+    },[])
     
+    const shuffledAnswer = (answer) => {
+        answer.sort(() => Math.random() - 0.5)
+    }
+
     return (<>
-        {props.data.map((question, index) => (
-            <div key={index} className="d-flex flex-row justify-content-center">
-                <p >{question.question}</p>
-                {/* <p>{question.incorrect_answers.map((answer) => (
-                    <li>{answer}</li>
-                ))}</p> */}
-                <p>{question.correct_answer}</p>
-            </div>
-        ))}
+
+    <button onClick={props.handleGameClick} className="btn btn-primary">Play Again</button>
     </>)
         
 }
